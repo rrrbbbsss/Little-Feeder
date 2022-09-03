ed9079d161b7c512a8167e445c91661326a12bec
Royce Strange <roycest@swbell.net>
2022-09-02 20:00:45 -0500

    add initial server

---
+++ server.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const express = require("express");
+const routes = require("./controllers");
+const dbconn = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+require("dotenv").config();
+SESSION_SECRET = process.env.SESSION_SECRET;
+
+const sess = {
+  secret: SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: dbconn,
+  }),
+};
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sess));
+// handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+dbconn.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Now listening"));
+});
