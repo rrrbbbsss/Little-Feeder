9f888581176afc48f5ff7ec5ee018897d908c3b1
amon1459 <amon.rashidi@yahoo.com>
2022-09-07 01:02:55 -0500

    html and css for articles page

---
+++ public/javascript/Articles.js
@@ -0,0 +1,29 @@
+{
+"count"; 1,
+"pages"; 1,
+"articles"; [
+{
+"-id": 1,
+"title": "blah",
+"published": "2022-09-06T03:15:10.0007",
+"ur1": "https://blah.blah.com",
+"userArticles" : [
+{
+"unread": true
+}
+],
+"feed": {
+"aid": 1,
+"url": "htto://www.reddit.com/.rss",
+"userFeeds": [
+{
+"description": 'asdfasdfsadf'
+}
+]
+}
+}
+]
+}
+
+
+console.log(fetch("https://blah.blah.com" ))
