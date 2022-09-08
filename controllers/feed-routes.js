d0893d8c534e555cf31b624d2234124beee72215
Royce Strange <roycest@swbell.net>
2022-09-07 22:28:01 -0500

    pass feed info to handlebars

--- controllers/feed-routes.js
+++ controllers/feed-routes.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 const dbconn = require("../config/connection");
 const { PUB, AAA, PAA } = require("../utils/auth");
+const { Feed, UserFeed } = require("../models");
 
-router.get("/", PAA, (req, res) => {
-  res.render("feed", { loggedIn: req.session.loggedIn });
+router.get("/", PAA, async (req, res) => {
+  const dbData = await Feed.getAll(req, { Feed, UserFeed });
+  const feeds = dbData.map((feed) => feed.get({ plain: true }));
+  res.render("feed", { loggedIn: req.session.loggedIn, feeds, feedPage: true });
 });
 
 module.exports = router;
